096a97373af7bc134b30d7218699a9450792539f
Martin Niederl <martin.niederl99@gmail.com>
2017-05-01 19:23:05 +0200

    Revert "Added ByteSizeConverter"
    This reverts commit b845e75bd78a6691b76279d8e6cf70d186a2d129.

--- 03_SourceCode/WindowsAutomatedSimplifier/FileSystem/FileSystemLogic.cs
+++ 03_SourceCode/WindowsAutomatedSimplifier/FileSystem/FileSystemLogic.cs
@@ -1,78 +1,29 @@
 ﻿using System;
-using System.Collections.Generic;
 using System.IO;
 using System.Linq;
-using System.Threading.Tasks;
-using Microsoft.VisualBasic.FileIO;
 
 namespace WindowsAutomatedSimplifier.FileSystem
 {
     internal class FileSystemLogic
     {
-        //TODO add ByteSize... to convert sizes
-
-        /// <summary>
-        /// Delete empty directories in a given root directory.
-        /// </summary>
-        /// <param name="rootDirectory">Root directory to start.</param>
-        public static void DeleteEmptyDirectories(string rootDirectory)
+        public static void DeleteEmptyDirectories(string dir)
         {
-            if (string.IsNullOrEmpty(rootDirectory)) throw new ArgumentException(@"Starting rootDirectory is a null reference or an empty string", nameof(rootDirectory));
+            if (string.IsNullOrEmpty(dir)) throw new ArgumentException(@"Starting directory is a null reference or an empty string", nameof(dir));
 
             try
             {
-                foreach (string d in Directory.EnumerateDirectories(rootDirectory))
+                foreach (string d in Directory.EnumerateDirectories(dir))
                     DeleteEmptyDirectories(d);
-
-                if (!Directory.EnumerateFileSystemEntries(rootDirectory).Any())
+                
+                if (!Directory.EnumerateFileSystemEntries(dir).Any())
                 {
-                    try { Directory.Delete(rootDirectory); }
+                    try { Directory.Delete(dir); }
                     catch (UnauthorizedAccessException) { }
                     catch (DirectoryNotFoundException) { }
                     catch (Exception) { }
                 }
             }
             catch (UnauthorizedAccessException) { }
-        }
-
-        /// <summary>
-        /// Moves a list of files to a given rootDirectory.
-        /// </summary>
-        /// <param name="fileInfos">List of the files.</param>
-        /// <param name="directory">Path of the new rootDirectory.</param>
-        public bool MoveFiles(IEnumerable<FileInfo> fileInfos, string directory)
-        {
-            if (!Directory.Exists(directory))
-            {
-                Console.Error.WriteLine("Given directory does not exist!");
-                return false;
-            }
-
-            try { Parallel.ForEach(fileInfos, fileInfo => fileInfo.MoveTo(directory + fileInfo.Name + fileInfo)); }
-            catch (Exception e)
-            {
-                Console.Error.WriteLine(e.Message);
-                return false;
-            }
-            return true;
-        }
-
-        /// <summary>
-        /// Deletes a list of files.
-        /// </summary>
-        /// <param name="fileInfos">List of the files.</param>
-        /// <param name="toRecycleBin">Option to move the files to the recyle bin instead of deleting them instantly.</param>
-        public void DeleteFiles(IEnumerable<FileInfo> fileInfos, bool toRecycleBin)
-        {
-            Parallel.ForEach(fileInfos, fileInfo =>
-            {
-                if (toRecycleBin)
-                    Microsoft.VisualBasic.FileIO.FileSystem.DeleteFile(fileInfo.FullName, UIOption.OnlyErrorDialogs, RecycleOption.SendToRecycleBin,
-                        UICancelOption.ThrowException);
-                else
-                    Microsoft.VisualBasic.FileIO.FileSystem.DeleteFile(fileInfo.FullName, UIOption.OnlyErrorDialogs, RecycleOption.SendToRecycleBin,
-                        UICancelOption.ThrowException);
-            });
         }
     }
 }
