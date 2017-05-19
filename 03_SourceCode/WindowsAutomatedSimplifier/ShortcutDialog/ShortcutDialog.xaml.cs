32000e330eb87a0197bf02a2c5b1141608c84419
David Eilmsteiner <d.eilmsteiner@students.htl-leonding.ac.at>
2017-05-19 12:30:20 +0200

    Improved ShortcutDialog

--- 03_SourceCode/WindowsAutomatedSimplifier/ShortcutDialog/ShortcutDialog.xaml.cs
+++ 03_SourceCode/WindowsAutomatedSimplifier/ShortcutDialog/ShortcutDialog.xaml.cs
@@ -1,8 +1,20 @@
-﻿using System;
-using System.Windows;
-using System.Diagnostics;
+﻿using GlobalHotkeyExampleForm;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
 using System.Threading.Tasks;
-using static System.Environment;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+using WindowsAutomatedSimplifier.DeCompress;
+using WindowsAutomatedSimplifier.Repository;
 
 namespace WindowsAutomatedSimplifier.ShortcutDialog
 {
@@ -11,54 +23,99 @@ namespace WindowsAutomatedSimplifier.ShortcutDialog
     /// </summary>
     public partial class ShortcutDialog : Window
     {
-        public GlobalHotkeys GHInstance { get; }
+        private GlobalHotkeys gH;
 
         public ShortcutDialog()
         {
             InitializeComponent();
-            GHInstance = new GlobalHotkeys(this);
+            gH = new GlobalHotkeys(this);
         }
 
-        public void SH01_Click(object sender, RoutedEventArgs e)
+        public void textBox01_MouseDoubleClick(object sender, RoutedEventArgs e)
         {
-            RunCommand(GetFolderPath(SpecialFolder.ApplicationData));
+            System.Diagnostics.Process.Start("explorer.exe", textBox01.Text);
         }
 
-        public void SH02_Click(object sender, RoutedEventArgs e)
+        public void textBox02_MouseDoubleClick(object sender, RoutedEventArgs e)
         {
-            RunCommand(@"C:\ProgramData");
+            System.Diagnostics.Process.Start("explorer.exe", textBox02.Text);
         }
 
-        public void SH03_Click(object sender, RoutedEventArgs e)
+        public void textBox03_MouseDoubleClick(object sender, RoutedEventArgs e)
         {
-            RunCommand(GetFolderPath(SpecialFolder.Desktop));
+            System.Diagnostics.Process.Start("explorer.exe", textBox03.Text);
         }
 
-        public void SH04_Click(object sender, RoutedEventArgs e)
+        public void textBox04_MouseDoubleClick(object sender, RoutedEventArgs e)
         {
-            RunCommand(@"C:\ProgramData");
+            System.Diagnostics.Process.Start("explorer.exe", textBox04.Text);
         }
 
-        public void SH05_Click(object sender, RoutedEventArgs e)
+        public void textBox05_MouseDoubleClick(object sender, RoutedEventArgs e)
         {
-            RunCommand(@"C:\ProgramData");
-        }
-
-        private static void RunCommand(string arguments)
-        {
-            try
-            {
-                Task.Factory.StartNew(() => Process.Start("explorer.exe", arguments));
-            }
-            catch (Exception e)
-            {
-                Console.Error.WriteLine(e.Message);
-            }
+            System.Diagnostics.Process.Start("explorer.exe", textBox05.Text);
         }
 
         public void Window_Closing(object sender, System.ComponentModel.CancelEventArgs e)
         {
-            GHInstance.WindClose();
+            Console.WriteLine(Environment.GetFolderPath(Environment.SpecialFolder.ApplicationData));
+
+            gH.ExampleForm_FormClosing();
+            String[] allPaths = new string[5];
+            allPaths[0] = textBox01.Text;
+            allPaths[1] = textBox02.Text;
+            allPaths[2] = textBox03.Text;
+            allPaths[3] = textBox04.Text;
+            allPaths[4] = textBox05.Text;
+
+            File.WriteAllLines(@"C:\Users\david\Documents\WinAutomatedSimplifier\WinAutomatedSimplifier\03_SourceCode\WindowsAutomatedSimplifier\ShortcutDialog\ShortcutList.txt", allPaths);
+        }
+
+        private void Window_Initialized(object sender, EventArgs e)
+        {
+            //Read Paths from file.
+            string[] values = new string[5];
+            values = File.ReadAllLines(@"C:\Users\david\Documents\WinAutomatedSimplifier\WinAutomatedSimplifier\03_SourceCode\WindowsAutomatedSimplifier\ShortcutDialog\ShortcutList.txt");
+
+            textBox01.Text = values[0];
+            textBox02.Text = values[1];
+            textBox03.Text = values[2];
+            textBox04.Text = values[3];
+            textBox05.Text = values[4];
+        }
+
+        private void GetFolderPath(TextBox tb)
+        {
+            var fbd = new System.Windows.Forms.FolderBrowserDialog();
+            if (fbd.ShowDialog() == System.Windows.Forms.DialogResult.OK)
+            {
+                tb.Text = fbd.SelectedPath;
+            }
+        }
+
+        private void sH05_Click(object sender, RoutedEventArgs e)
+        {
+            GetFolderPath(textBox05);
+        }
+
+        private void sH04_Click(object sender, RoutedEventArgs e)
+        {
+            GetFolderPath(textBox04);
+        }
+
+        private void sH03_Click(object sender, RoutedEventArgs e)
+        {
+            GetFolderPath(textBox03);
+        }
+
+        private void sH02_Click(object sender, RoutedEventArgs e)
+        {
+            GetFolderPath(textBox02);
+        }
+
+        private void sH01_Click(object sender, RoutedEventArgs e)
+        {
+            GetFolderPath(textBox01);
         }
     }
 }
